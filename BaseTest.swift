d62369152eff5811327803b0c22d3e4c2f20b19f
Artem Belov <77391924+barsolomey@users.noreply.github.com>
2023-07-04 15:27:55 +0200

    add BaseTest class

---
+++ BaseTest.swift
@@ -0,0 +1,18 @@
+//
+//  BaseTest.swift
+//  VeloxDashUITests
+//
+//  Created by Artem Belov on 30.05.2023.
+//
+
+import XCTest
+
+class BaseTest: XCTestCase {
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+}
