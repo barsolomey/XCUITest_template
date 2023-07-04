f0fb88b43f273483a93dc6bcdedd58f5919812a0
Artem Belov <77391924+barsolomey@users.noreply.github.com>
2023-07-04 15:28:15 +0200

    add TimeOut enum

---
+++ Enums/TimeOut.swift
@@ -0,0 +1,14 @@
+//
+//  TimeOut.swift
+//  Scheme name
+//
+//  Created by Artem Belov on 30.05.2023.
+//
+
+import XCTest
+
+enum TimeOut: Double {
+    case short = 5
+    case medium = 10
+    case long = 30
+}
